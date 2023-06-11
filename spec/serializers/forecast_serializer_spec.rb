a506fdc3c6ff61429cc2ea788c01c69861e96923
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-06-11 13:36:59 -0400

    create forecast serializer spec

---
+++ spec/serializers/forecast_serializer_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe ForecastSerializer do
+  describe 'instance methods' do
+    it 'properly serializes forecast data' do
+      serialized = ForecastSerializer.new(ForecastService.get_forecast('Cincinatti, OH')).serialize_forecast
+      current = serialized[:data][:attributes][:current_weather]
+      daily = serialized[:data][:attributes][:daily_weather]
+      hourly = serialized[:data][:attributes][:hourly_weather]
+      
+      expect(serialized).to be_a(Hash)
+      expect(serialized[:data]).to be_a(Hash)
+      expect(serialized[:data][:id]).to eq(nil)
+      expect(serialized[:data][:type]).to eq('forecast')
+      expect(serialized[:data][:attributes]).to be_a(Hash)
+
+      expect(current).to be_a(Hash)
+      expect(current[:last_updated]).to be_a(String)
+      expect(current[:temperature]).to be_a(Float)
+      expect(current[:feels_like]).to be_a(Float)
+      expect(current[:humidity]).to be_a(Integer)
+      expect(current[:uvi]).to be_a(Float)
+      expect(current).to_not have_key(:temp_c)
+      expect(current).to_not have_key(:feelslike_c)
+      expect(current).to_not have_key(:vis_km)
+      expect(current).to_not have_key(:wind_kph)
+      expect(current).to_not have_key(:wind_dir)
+      expect(current).to_not have_key(:wind_degree)
+      expect(current).to_not have_key(:pressure_mb)
+      expect(current).to_not have_key(:pressure_in)
+      expect(current).to_not have_key(:precip_mm)
+      expect(current).to_not have_key(:precip_in)
+
+      expect(daily).to be_a(Array)
+      expect(daily[0]).to be_a(Hash)
+      expect(daily[0][:date]).to be_a(String)
+      expect(daily[0][:sunrise]).to be_a(String)
+      expect(daily[0][:sunset]).to be_a(String)
+      expect(daily[0][:max_temp]).to be_a(Float)
+      expect(daily[0][:min_temp]).to be_a(Float)
+      expect(daily[0][:condition]).to be_a(String)
+      expect(daily[0][:icon]).to be_a(String)
+      expect(daily[0]).to_not have_key(:maxtemp_c)
+      expect(daily[0]).to_not have_key(:mintemp_c)
+      expect(daily[0]).to_not have_key(:avgtemp_c)
+      expect(daily[0]).to_not have_key(:maxwind_kph)
+      expect(daily[0]).to_not have_key(:totalprecip_mm)
+      expect(daily[0]).to_not have_key(:avgvis_km)
+
+      expect(hourly).to be_a(Array)
+      expect(hourly[0]).to be_a(Hash)
+      expect(hourly[0][:time]).to be_a(String)
+      expect(hourly[0][:temperature]).to be_a(Float)
+      expect(hourly[0][:condition]).to be_a(String)
+      expect(hourly[0][:icon]).to be_a(String)
+      expect(hourly[0]).to_not have_key(:temp_c)
+      expect(hourly[0]).to_not have_key(:wind_kph)
+      expect(hourly[0]).to_not have_key(:wind_dir)
+      expect(hourly[0]).to_not have_key(:wind_degree)
+      expect(hourly[0]).to_not have_key(:pressure_mb)
+      expect(hourly[0]).to_not have_key(:pressure_in)
+    end
+  end
+end
