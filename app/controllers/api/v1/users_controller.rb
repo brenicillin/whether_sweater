bbc4c867c7bf1d5ace108cbf5763622d0a678762
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-06-12 19:54:51 -0400

    update users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: UsersSerializer.creation(user), status: 201
+      render json: UsersSerializer.serialize(user), status: 201
     else
       render json: { error: user.errors.full_messages.to_sentence }, status: 400
     end
