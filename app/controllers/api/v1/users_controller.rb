dd1df41cb787e1c75e7c4816928fc888e4ded58d
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-06-12 15:31:14 -0400

    create users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: UsersSerializer.creation(user), status: 201
+    else
+      render json: { error: user.errors.full_messages.to_sentence }, status: 400
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
